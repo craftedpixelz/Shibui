76da858c100bcd55a67a034a3815c1b4ab94db87
Abid <Abid@Abids-MacBook-Air.local>
2012-12-21 01:05:26 +0000

    Initial commit

---
+++ resource/js/global.js
@@ -0,0 +1,21 @@
+/* -----------------------------------------------  
+
+Site Name:
+File:
+Author:
+Copyright (c):
+  
+------------------------------------------------ */
+
+// Create a namespace to prevent code conflict
+var myApp = myApp || {};
+
+// Declare methods for use in your App
+myApp.myFunction = function(){
+	// Logic here
+};
+
+// Execute on DOM Ready
+$(function(){
+	myApp.myFunction();
+});
---
+++ resource/js/plugins.js
@@ -0,0 +1,10 @@
+/* -----------------------------------------------  
+
+Site Name:
+File:
+Author:
+Copyright (c):
+  
+------------------------------------------------ */
+
+// Place JS helper plugins here. Minify plugins for optimal performance.
